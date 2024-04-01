8b4eab420bd5c0eaad92c4191786e808dd438e62
LeafmanZ <zielemanj@gmail.com>
2024-04-01 10:52:02 -0400

    fewfwe

--- create_bridge.py
+++ create_bridge.py
@@ -8,18 +8,15 @@ def execute_command(command):
         print(f"Error executing {command}: {e}")
 
 commands = [
-    "sudo ifconfig eth1 up",
-    "sudo ifconfig eth2 up",
+    "sudo ifconfig ens192 up",
+    "sudo ifconfig ens224 up",
     "sudo ip link add br0 type bridge",
     "sudo ip link set br0 up",
-    "sudo ip link set dev eth1 master br0",
-    "sudo ip link set dev eth2 master br0",
-    "sudo dhclient br0",
+    "sudo ip link set dev ens192 master br0",
+    "sudo ip link set dev ens224 master br0",
     "sudo brctl show",
-    "sudo brctl addif br0 eth1",
-    "sudo brctl addif br0 eth2",
-    "sudo echo 1 > /proc/sys/net/ipv4/ip_forward",
-    "sudo ip addr add 10.20.10.7/24 dev br0"
+    "sudo brctl addif br0 ens192",
+    "sudo brctl addif br0 ens224",
 ]
 
 for cmd in commands:
